a762aab7123819cdfcebb162c4f4e783ab1adee5
Paula <paulajardimf@gmail.com>
2022-09-05 19:16:13 -0300

    tarefas semana 1 concluídas

---
+++ script.js
@@ -0,0 +1,55 @@
+const nomeExpansao1 = "The Sims 4: Ao Trabalho";
+const anoLancamento1 = 2015;
+const adicionaNovoMundo1 = true;
+
+const nomeExpansao2 = "The Sims 4: Junte-se à Galera";
+const anoLancamento2 = 2015;
+const adicionaNovoMundo2 = true;
+
+const nomeExpansao3 = "The Sims 4: Vida na Cidade";
+const anoLancamento3 = 2016;
+const adicionaNovoMundo3 = true;
+
+const nomeExpansao4 = "The Sims 4: Gatos e Cães";
+const anoLancamento4 = 2017;
+const adicionaNovoMundo4 = true;
+
+const nomeExpansao5 = "The Sims 4: Estações";
+const anoLancamento5 = 2018;
+const adicionaNovoMundo5 = false;
+
+const nomeExpansao6 = "The Sims 4: Rumo à Fama";
+const anoLancamento6 = 2018;
+const adicionaNovoMundo6 = true;
+
+const nomeExpansao7 = "The Sims 4: Ilhas Tropicais";
+const anoLancamento7 = 2019;
+const adicionaNovoMundo7 = true;
+
+const nomeExpansao8 = "The Sims 4: Vida Universitária";
+const anoLancamento8 = 2019;
+const adicionaNovoMundo8 = true;
+
+const nomeExpansao9 = "The Sims 4: Vida Sustentável";
+const anoLancamento9 = 2020;
+const adicionaNovoMundo9 = true;
+
+const nomeExpansao10 = "The Sims 4: Diversão na Neve";
+const anoLancamento10 = 2020;
+const adicionaNovoMundo10 = true;
+
+const nomeExpansao11 = "The Sims 4: Vida Campestre";
+const anoLancamento11 = 2021;
+const adicionaNovoMundo11 = true;
+
+const nomeExpansao12 = "The Sims 4: Vida no Ensino Médio";
+const anoLancamento12 = 2022;
+const adicionaNovoMundo12 = true;
+
+let mediaAnoLancamento = (anoLancamento1 + anoLancamento2 + anoLancamento3 + anoLancamento4 + anoLancamento5 + anoLancamento6 + anoLancamento7 + anoLancamento8 + anoLancamento9 + anoLancamento10 + anoLancamento11 + anoLancamento12) / 12;
+console.log(mediaAnoLancamento);
+
+let anosLancamento = [anoLancamento1, anoLancamento2, anoLancamento3, anoLancamento4, anoLancamento5, anoLancamento6, anoLancamento7, anoLancamento8, anoLancamento9, anoLancamento10, anoLancamento11, anoLancamento12];
+
+let verificaNovoMundo = adicionaNovoMundo1 && adicionaNovoMundo2 && adicionaNovoMundo3 && adicionaNovoMundo4 && adicionaNovoMundo5 && adicionaNovoMundo6 && adicionaNovoMundo7 && adicionaNovoMundo8 && adicionaNovoMundo9 && adicionaNovoMundo10 && adicionaNovoMundo11 && adicionaNovoMundo12;
+console.log(verificaNovoMundo);
